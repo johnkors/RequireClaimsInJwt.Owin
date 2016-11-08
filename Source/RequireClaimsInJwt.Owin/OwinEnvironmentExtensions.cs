dd750d618b9c744719c620e84f72db0454bc9af3
john4ice <john.korsnes@ice.net>
2016-11-08 17:46:45 +0100

    Sep classes

--- Source/RequireClaimsInJwt.Owin/OwinEnvironmentExtensions.cs
+++ Source/RequireClaimsInJwt.Owin/OwinEnvironmentExtensions.cs
@@ -4,24 +4,6 @@ using System.IdentityModel.Tokens;
 
 namespace RequireClaimsInJwt.Owin
 {
-    internal static class JwtSecurityTokenExtensions
-    {
-        internal static IEnumerable<string> CheckRequirements(this JwtSecurityToken token, IEnumerable<ClaimRequirement> requirements)
-        { 
-            var errors = new List<string>();
-            foreach (var requirement in requirements)
-            {
-                var fulfillsRequirement = requirement.Verify(token.Claims);
-                if (!fulfillsRequirement)
-                {
-                    errors.Add(requirement.ErrorMsg);
-                }
-            }
-
-            return errors;
-        }
-    }
-
     internal static class OwinEnvironmentExtensions
     {
 
