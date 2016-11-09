1c0d6794fb7d27fca71ba50e6f50888a84fdd810
john4ice <john.korsnes@ice.net>
2016-11-09 16:05:42 +0100

    Add xunit testrun to build script

--- buildnuget.bat
+++ buildnuget.bat
@@ -1,8 +1,36 @@
+@echo off
 set csproj=source\RequireClaimsInJwt.Owin\RequireClaimsInJwt.Owin.csproj
 set version=1.2.0
 set msbuildexe=
 for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework\v4*) do set msbuildexe=%%D\MSBuild.exe
+set xunitrunnerpath=source\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe
 
 nuget restore %csproj% -SolutionDirectory source\
-%msbuildexe% source\Solution.sln /p:Configuration=Release
-nuget pack %csproj% -Version %version% -OutputDirectory dist\ -Properties Configuration=Release
+
+
+if %ERRORLEVEL% EQU 0 (
+   %msbuildexe% source\Solution.sln /p:Configuration=Release
+) else (
+   echo **************** Nuget restore failed ****************
+   exit /b %errorlevel%
+)
+
+if %ERRORLEVEL% EQU 0 (
+   %xunitrunnerpath% source\RequireClaimsInJwt.Owin.Tests\bin\Release\RequireClaimsInJwt.Owin.Tests.dll
+) else (
+   echo **************** MSBuild failed *****************
+   exit /b %errorlevel%
+)
+
+if %ERRORLEVEL% EQU 0 (
+   nuget pack %csproj% -Version %version% -OutputDirectory dist\ -Properties Configuration=Release
+) else (
+   echo **************** XUnit tests failed! ****************
+   exit /b %errorlevel%
+)
+
+echo \n
+echo *************************************************
+echo **************** BUILD SUCCESS! *****************
+echo *************************************************
+
